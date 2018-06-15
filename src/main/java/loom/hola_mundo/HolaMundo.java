41884ed7cab6b1ce62dc6f21f0a57a7b56539f05
fedemarkoo <fede.markoo@gmail.com>
2018-06-15 14:11:48 -0300

    Primer consigna resuelta

--- src/main/java/loom/hola_mundo/HolaMundo.java
+++ src/main/java/loom/hola_mundo/HolaMundo.java
@@ -2,7 +2,13 @@ package loom.hola_mundo;
 
 public class HolaMundo {
 
-    public String saludar(final String persona) {
-        throw new RuntimeException("¡Implementar este método!");
-    }
+	public String saludar(final String persona) {
+		String nuevo = ""; // Creo una String nueva para usar de "buffer" para poner en CamelCase
+		for (String cad : persona.split(" ")) { // separo el nombre en espacios para obtener los nombres y apellidos
+			nuevo += (cad.charAt(0) + "").toUpperCase(); // con esto obtengo la primer letra y la pongo en mayuscula
+			nuevo += cad.substring(1).toLowerCase(); // con esto obtengo el resto del nombre para poner en minuscula
+			nuevo += " "; // aca agrego el espacio que lo borra el split
+		}
+		return "�Hola, " + nuevo.trim() + "!"; // formateo la salida con el nombre y todo como pide el ejercicio
+	}
 }
--- src/test/java/loom/hola_mundo/HolaMundoTests.java
+++ src/test/java/loom/hola_mundo/HolaMundoTests.java
@@ -4,9 +4,22 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class HolaMundoTests {
-    @Test
-    public void queSaludaCorrectamente() {
-        HolaMundo holaMundo = new HolaMundo();
-        Assert.assertEquals("Â¡Hola, Kent Beck!", holaMundo.saludar("Kent Beck"));
-    }
+	@Test
+	public void queSaludaCorrectamente() {
+		HolaMundo holaMundo = new HolaMundo();
+		Assert.assertEquals("¡Hola, Kent Beck!", holaMundo.saludar("Kent Beck"));
+		Assert.assertEquals("¡Hola, Fede Markoo!", holaMundo.saludar("Fede Markoo"));
+		Assert.assertEquals("¡Hola, Lucas Videla!", holaMundo.saludar("lucas videla"));
+	}
+
+	@Test
+	public void queSaludaCorrectamenteSimpsons() {
+		HolaMundo holaMundo = new HolaMundo();
+		Assert.assertEquals("¡Hola, Georgia!", holaMundo.saludar("Georgia"));
+		Assert.assertEquals("¡Hola, Texas!", holaMundo.saludar("Texas"));
+		Assert.assertEquals("¡Hola, Aam.. Brooklyn!", holaMundo.saludar("aam.. Brooklyn"));
+		Assert.assertEquals("¡Hola, Rusia!", holaMundo.saludar("Rusia"));
+		Assert.assertEquals("¡Hola, Escocia!", holaMundo.saludar("Escocia"));
+		Assert.assertEquals("¡Hola, Homero!", holaMundo.saludar("HOMERO"));
+	}
 }
