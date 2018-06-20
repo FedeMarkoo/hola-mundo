025280095c0aaab52c160ac07ae9527a750998dd
fedemarkoo <fede.markoo@gmail.com>
2018-06-20 17:39:18 -0300

    really??

--- src/main/java/loom/hola_mundo/HolaMundo.java
+++ src/main/java/loom/hola_mundo/HolaMundo.java
@@ -1,4 +1,4 @@
-
+ï»¿
 package loom.hola_mundo;
 
 /**
@@ -9,20 +9,20 @@ public class HolaMundo {
 
     /**
      * Saluda a la persona.
-     * Este método recibe por parametro el nombre de la persona
+     * Este mÃ©todo recibe por parametro el nombre de la persona
      * y devuelve un String saludando por su nombre.
      *
      * @param persona a saludar.
      * @return persona saludada.
      */
 
-	public String saludar(final String persona) {
+public String saludar(final String persona) {
 		String nuevo = "";
 		for (String cad : persona.split(" ")) {
-			nuevo += (cad.charAt(0) + "").toUpperCase(); 
-			nuevo += cad.substring(1).toLowerCase(); 
-			nuevo += " "; 
+			nuevo += (cad.charAt(0) + "").toUpperCase();
+			nuevo += cad.substring(1).toLowerCase();
+			nuevo += " ";
 		}
-		return "¡Hola, " + nuevo.trim() + "!"; 
+		return "Â¡Hola, " + nuevo.trim() + "!";
 	}
 }
